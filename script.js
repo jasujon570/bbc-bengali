217faee3398f0ff018e8f7ee70113f7e5b2bfcfb
sujon <jasujon570@gmail.com>
2025-10-26 23:54:17 +0600

    load nav title

--- script.js
+++ script.js
@@ -15,8 +15,20 @@ const loadCategory = () => {
 const showCategory = (categories) => {
   categories.forEach((cat) => {
     categoryContainer.innerHTML += `
-        <li id="${cat.id}" class="hover:border-b-4 hover:border-red-600 cursor-pointer">${cat.title}</li>
+        <li id="${cat.id}" class="hover:border-b-4 hover:border-red-600 border-red-600 cursor-pointer">${cat.title}</li>
         `;
+  });
+
+  categoryContainer.addEventListener("click", (e) => {
+    const allLi = document.querySelectorAll("li");
+
+    allLi.forEach((li) => {
+      li.classList.remove("border-b-4");
+    });
+
+    if (e.target.localName === "li") {
+      e.target.classList.add("border-b-4");
+    }
   });
 };
 
